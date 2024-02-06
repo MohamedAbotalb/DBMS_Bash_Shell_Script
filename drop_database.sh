ed6a2f8886cb1ccadb86a90cf1d58416ef24b25e
MohamedAbotalb <mohamed.abotalb277@gmail.com>
2024-02-06 23:09:03 +0200

    Add drop_database.sh file

--- drop_database.sh
+++ drop_database.sh
@@ -1,40 +1,54 @@
 #!/bin/bash
 
 read -p "Enter name of database: " database_name
+
 result=`./check_valid_value.sh $database_name`
 
-database_path="./DataBases/$database_name"
+database_path="./Databases/$database_name"
 
-if [ ! -d "$database_path" ]; 
+# check if database name is not valid
+if [[ $result ]];
+then
+  echo "-------------------------------------"
+  echo $result
+  echo "-------------------------------------"
 
+  ./drop_database.sh
+
+# Check if the database name doesn't match anyone in the databases
+elif [ ! -d "$database_path" ]; 
 then
   echo "--------------------------------------"
   echo "Database $database_name doesn't exist!"
   echo "--------------------------------------"
 
-    ./main.sh
+  ./drop_database.sh
 
 else
-  read -p "Are you Sure You Want To delete $database_name Database? y/n " $choice ;
+  read -p "Are you Sure You Want To delete $database_name Database? y/n " choice
+
 	case $choice in
-		 [Yy]* ) 
+
+		[Yy] ) 
 			rm -r "$database_path"
-				echo "---------------------------------------------"
-         		echo "Database $database_name deleted successfully!"
-         		echo "---------------------------------------------"
+			echo "---------------------------------------------"
+			echo "Database $database_name deleted successfully!"
+			echo "---------------------------------------------"
 			;;
-		 [Nn]* )
-      			echo "----------------------"
+
+		[Nn] )
+			echo "----------------------"
 			echo "Cancel delete!"
-      			echo "----------------------"
+			echo "----------------------"
 			;;
-		 * ) 
-		 	echo "--------------------------"
+
+		* ) 
+			echo "--------------------------"
 			echo "Please choose y/n"
 			echo "--------------------------"
 			;;
 	esac
   
-    ./main.sh
+  ./main.sh
 
 fi
