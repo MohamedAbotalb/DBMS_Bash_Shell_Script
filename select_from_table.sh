f53601121b597effc069c9c423c6f9cad09e49b3
MohamedAbotalb <mohamed.abotalb277@gmail.com>
2024-02-08 00:44:10 +0200

    Add select_from_table.sh file

--- select_from_table.sh
+++ select_from_table.sh
@@ -1,31 +1,156 @@
 #!/bin/bash
 
-# Ask user to input an ID to update
-read -p "Enter the ID you want to update: " selected_id
+database_name=$1
+database_dir=./Databases/$database_name
+metadata_dir=$database_dir/.metadata
 
-# Validate if the selected ID exists in the file
-if ! grep -q "^$selected_id :" ./Databases/iti/students/.metadata; then
-    echo "Invalid ID. Exiting."
-    exit 1
+echo "-------------------------------------"
+echo "Available tables in $database_name:"
+ls $database_dir
+echo "-------------------------------------"
+
+read -p "Enter the table name to select from: " table_name
+
+result=`./check_valid_value.sh $table_name`
+
+# check if the table name is not valid
+if [[ $result ]];
+then
+  echo "-------------------------------------"
+  echo $result
+  echo "-------------------------------------"
+
+  ./select.sh $database_name
+
+# Check if the table name isn't present in the selected database
+elif [[ ! -f $database_dir/$table_name ]];
+then
+  echo "-------------------------------------"
+  echo "$table_name isn't present, please enter a new name"
+  echo "-------------------------------------"
+
+  ./select.sh $database_name
+
+else
+  table_name_path=$database_dir/$table_name
+  table_meta_path=$database_dir/.metadata/$table_name
+
+  PS3="Choose what you want: "
+  select choice in "Select All" "Select Specific Row"
+  do
+    case $REPLY in
+      # Select all rows
+      1)
+        # Check if the table contains data or not
+        if [[ ! -s "$table_name_path" ]]; 
+        then
+          echo "-------------------------------------"
+          echo "This table is empty."
+          echo "-------------------------------------"
+        else
+          echo "-------------------------------------"
+          head -1 $table_meta_path.meta
+          echo "-------------------------------------"
+          cat $table_name_path
+          echo "-------------------------------------"
+        fi
+
+        ./table_menu.sh $database_name
+        ;;
+
+      # Select specific row
+      2)
+        if [[ ! -s "$table_name_path" ]]; 
+        then
+          echo "-------------------------------------"
+          echo "This table is empty."
+          echo "-------------------------------------"
+        else
+          # Get the primary key value from the metadata file
+          meta=`awk 'NR==2 {print}' $table_meta_path.meta`
+          pk=$(echo "$meta" | awk -F: '{print $2}')
+
+          # Create an array to get the columns names from meta file
+          metadata=($(awk -F: 'NR==1 {for(i=1;i<=NF;i++) print $i}' $table_meta_path.meta))
+
+          # Create an array to get the columns datatypes from dtype file
+          datatypes=($(awk -F: 'NR==1 {for(i=1;i<=NF;i++) print $i}' $table_meta_path.dtype))
+
+          # Read the second line and get the index of the value after ":"
+          for((i=0; i < ${#metadata[@]}; i++)) 
+          do
+            if [[ "$pk" = "${metadata[$i]}" ]];
+            then
+              index=$i
+              break
+            fi
+          done
+
+          # Get the datatype value for the PK
+          dtype="${datatypes[$index]}"
+
+          while true;
+          do
+            # Ask the user to enter the value of the primary key
+            read -p "Enter the value of the primary key [$pk]: " PK
+
+            if [[ $dtype = "int" && "$PK" = +([0-9]) || $dtype = "string" && "$PK" = +([a-zA-Z@.]) ]]
+            then 
+              
+              # Get the PK values to check if the entered value is present or not
+              pk_values=()
+              while IFS=: read -r -a fields; 
+              do
+                pk_values+=("${fields[$index]}")
+              done < "$table_name_path"
+
+              # Check if the PK value is present or not
+              flag=0
+              for val in "${pk_values[@]}"; 
+              do
+                if [[ "$PK" = "$val" ]]; 
+                then
+                  flag=1
+                  break
+                fi
+              done
+  
+              if [[ $flag = 0 ]];
+              then
+                echo "-------------------------------------"
+                echo "The value of $pk isn't present in the table, enter a new value"
+                echo "-------------------------------------"
+
+              else
+                while IFS=: read -r -a fields; 
+                do
+                  if [[ "${fields[$index]}" = "$PK" ]]; 
+                  then
+                    # Display the row
+                    echo "-------------------------------------"
+                    head -1 $table_meta_path.meta
+                    echo "-------------------------------------"
+                    echo "${fields[*]}" | sed 's/ /:/g' # sed replaces space with :
+                    echo "-------------------------------------" 
+                    break 2
+                  fi
+                done < "$table_name_path"
+              fi
+
+            else
+              echo "-------------------------------------"
+              echo "The value of $pk is invalid, enter a new value"
+              echo "-------------------------------------"
+            fi
+          done
+        fi
+
+        ./table_menu.sh $database_name
+        ;;
+
+      *)
+        echo "$REPLY is not one of the choices"
+        ;;
+    esac
+  done
 fi
-
-# Display the selected row
-echo "Selected row:"
-awk -F ' : ' -v id="$selected_id" '$1 == id {print $0}' file
-
-# Ask user which field to update
-read -p "Which field do you want to update : " field_to_update
-
-# Validate the field input
-if [ "$field_to_update" != "name" ] && [ "$field_to_update" != "email" ]; then
-    echo "Invalid field. Exiting."
-    exit 1
-fi
-
-# Ask user for the new value
-read -p "Enter the new $field_to_update: " new_value
-
-# Update the data in the file
-awk -F ' : ' -v id="$selected_id" -v field="$field_to_update" -v value="$new_value" 
-
-echo "Data updated successfully."
