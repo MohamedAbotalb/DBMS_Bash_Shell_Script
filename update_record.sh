6217783a39ebad72eb184346d23fe0ada7801c1c
MohamedAbotalb <mohamed.abotalb277@gmail.com>
2024-02-10 19:45:45 +0200

    Refactor table features files

--- update_record.sh
+++ update_record.sh
@@ -2,116 +2,146 @@
 
 database_name=$1
 database_dir=./Databases/$database_name
+metadata_dir="$database_dir/.metadata"
 
-if [[ $(ls $database_dir) ]];
-then
-  echo "-------------------------------------"
-  echo "Available Tables in $database_name:"
-  ls $database_dir
-  echo "-------------------------------------"
+list_available_tables() {
+  if [[ $(ls "$database_dir") ]]; 
+  then
+    echo "-------------------------------------"
+    echo "--------- Available Tables ----------"
+    ls "$database_dir"
+    echo "-------------------------------------"
+  else
+    echo "-------------------------------------"
+    echo "There us no table found"
+    echo "-------------------------------------"
 
-else
-  echo "-------------------------------------"
-  echo "There is no tables found"
-  echo "-------------------------------------"
+    ./table_menu.sh "$database_name"
+  fi
+}
 
-  ./table_menu.sh $database_name
-fi
+get_table_name() {
+  read -p "Enter the table name: " table_name
+}
 
-read -p "Enter the table name you want to update: " table_name
+check_valid_table_name() {
+  local result=$(./check_valid_value.sh "$table_name")
 
-# check if the table name is not valid
-if [[ $result ]];
-then
-  echo "-------------------------------------"
-  echo $result
-  echo "-------------------------------------"
+  if [[ $result ]]; 
+  then
+    echo "-------------------------------------"
+    echo "$result"
+    echo "-------------------------------------"
 
-  ./update_record.sh $database_name
+    update_record
+  fi
+}
 
-# Check if the table name is present in the selected database
-elif [[ ! -f $database_dir/$table_name ]];
-then
-  echo "-------------------------------------"
-  echo "$table_name isn't present, please enter a new name"
-  echo "-------------------------------------"
+check_table_exists() {
+  if [[ ! -f "$database_dir/$table_name" ]]; 
+  then
+    echo "-------------------------------------"
+    echo "$table_name isn't present, please enter a new name"
+    echo "-------------------------------------"
 
-  ./update_record.sh $database_name
+    update_record
+  fi
+}
 
-else
+create_table_meta_path() {
   table_name_path=$database_dir/$table_name
-  table_meta_path=$database_dir/.metadata/$table_name 
+  table_meta_path=$metadata_dir/$table_name
+}
 
-  # Check if the file is empty
+check_data_presence() {
   if [[ ! -s "$table_name_path" ]]; 
   then
     echo "-------------------------------------"
     echo "This table is empty."
     echo "-------------------------------------"
-  else
-    # Get the primary key value from the metadata file
-    meta=`awk 'NR==2 {print}' $table_meta_path.meta`
-    pk=$(echo "$meta" | awk -F: '{print $2}')
 
-    # Create an array to get the columns names from meta file
-    metadata=($(awk -F: 'NR==1 {for(i=1;i<=NF;i++) print $i}' $table_meta_path.meta))
+    ./table_menu.sh $database_name
+  fi
+}
 
-    # Create an array to get the columns datatypes from dtype file
-    datatypes=($(awk -F: 'NR==1 {for(i=1;i<=NF;i++) print $i}' $table_meta_path.dtype))
+# Create an array to get the columns names and datatypes from meta and dtype files
+get_metadata_and_datatypes() {
+  metadata=($(awk -F: 'NR==1 {for(i=1;i<=NF;i++) print $i}' $table_meta_path.meta))
+  datatypes=($(awk -F: 'NR==1 {for(i=1;i<=NF;i++) print $i}' $table_meta_path.dtype))
+}
 
-    # Read the second line and get the index of the value after ":"
-    for((i=0; i < ${#metadata[@]}; i++)) 
-    do
-      if [[ "$pk" = "${metadata[$i]}" ]];
-      then
-        index=$i
-        break
-      fi
-    done
+# Get the primary key value from the metadata file
+get_pk_name() {
+  meta=$(awk 'NR==2 {print}' $table_meta_path.meta)
+  pk=$(echo "$meta" | awk -F: '{print $2}')
+}
 
-    # Create an array to get the real data of primary key column in table file to compare the inserted PK value with them to disallow duplicates
-    pk_values=($(awk -F: "{print \$($index+1)}" "$table_name_path"))
+# Read the second line and get the index of the primary key
+get_pk_index() {
+  index=""
+  for ((i=0; i < ${#metadata[@]}; i++)); 
+  do
+    if [[ "$pk" = "${metadata[$i]}" ]]; 
+    then
+      index=$i
+      break
+    fi
+  done
+}
 
-    # Get the datatype value for the PK
-    dtype="${datatypes[$index]}"
+# Create an array to get the real data of primary key column in table file to compare the inserted PK value with them to disallow duplicates
+get_pk_values() {
+  pk_values=($(awk -F: "{print \$($index+1)}" "$table_name_path"))
+}
 
-    while true;
-    do
-      # Ask the user to enter the value of the primary key
-      read -p "Enter the value of the primary key [$pk] to select a row: " PK
+# Get the datatype value for the PK
+get_pk_dtype() {
+  dtype="${datatypes[$index]}"
+}
 
-      if [[ $dtype = "int" && "$PK" = +([0-9]) || $dtype = "string" && "$PK" = +([a-zA-Z@.]) ]]
-      then
+get_target_row() {
+  while IFS=: read -r -a fields; 
+  do
+    if [[ "${fields[$index]}" = "$PK" ]]; 
+    then
+      # Display the row
+      old_row=$(echo "${fields[*]}" | sed 's/ /:/g') # sed replaces space with :
+      break
+    fi
+  done < "$table_name_path"
+}
 
-        # Check if the PK value is present or not
-        flag=0
-        for val in "${pk_values[@]}"; 
-        do
-          if [[ "$PK" = "$val" ]]; 
-          then
-            flag=1
-            break
-          fi
-        done
+get_old_row() {
+  while true;
+  do
+    # Ask the user to enter the value of the primary key
+    read -p "Enter the value of the primary key [$pk] to select a row: " PK
 
-        if [[ $flag = 0 ]];
+    if [[ $dtype = "int" && "$PK" = +([0-9]) || $dtype = "string" && "$PK" = +([a-zA-Z@.]) ]]
+    then
+      # Check if the PK value is present or not
+      flag=0
+      for val in "${pk_values[@]}"; 
+      do
+        if [[ "$PK" = "$val" ]]; 
         then
-          echo "-------------------------------------"
-          echo "The value of $pk isn't present in the table, enter a new value"
-          echo "-------------------------------------"
+          flag=1
+          break
+        fi
+      done
 
-        else
-          # Get the selected row to update its data
-          while IFS=: read -r -a fields; 
-          do
-            if [[ "${fields[$index]}" = "$PK" ]]; 
-            then
-              # Display the row
-              old_row=$(echo "${fields[*]}" | sed 's/ /:/g') # sed replaces space with :
-              
-            fi
-          done < "$table_name_path"
+      if [[ $flag = 0 ]];
+      then
+        echo "-------------------------------------"
+        echo "The value of $pk isn't present in the table, enter a new value"
+        echo "-------------------------------------"
 
+      else
+        # Get the selected row to update its data
+        get_target_row
+
+        while true;
+        do
           # Ask the user to enter the column name to update its value
           read -p "Enter the column name you want to update its value [ ${metadata[*]} ]: " column_name
 
@@ -172,29 +202,47 @@ else
               fi
             done
           fi
-        fi
-
-      else
-        echo "-------------------------------------"
-        echo "The value of $pk is invalid, enter a new value"
-        echo "-------------------------------------"
+        done
       fi
-    done
 
-    # old_row => new_row with the updated value
-    new_row=$(echo "$old_row" | awk -F: -v new_val="$value" -v pos="$position" '{$(pos+1) = new_val} 1' | sed 's/ /:/g')
+    else
+      echo "-------------------------------------"
+      echo "The value of $pk is invalid, enter a new value"
+      echo "-------------------------------------"
+    fi
+  done
+}
 
-    # Get the number of old_row 
-    row_number=$(awk -v pattern="$PK" '$0 ~ pattern {print NR; exit}' "$table_name_path")
+create_new_row() {
+  # old_row => new_row with the updated value
+  new_row=$(echo "$old_row" | awk -F: -v new_val="$value" -v pos="$position" '{$(pos+1) = new_val} 1' | sed 's/ /:/g')
 
-    # Remove the old_row from the table and then create the new_row and replace the new_row with the old_row
-    awk -v row_number="$row_number" -v new_row="$new_row" 'NR == row_number { print new_row; next } { print }' "$table_name_path" > temp.txt && mv temp.txt "$table_name_path"
+  # Get the number of old_row 
+  row_number=$(awk -v pattern="$PK" '$0 ~ pattern {print NR; exit}' "$table_name_path")
 
-    echo "-------------------------------------"
-    echo "The Row is Updated Successfully"
-    echo "-------------------------------------"
+  # Remove the old_row from the table and then create the new_row and replace the new_row with the old_row
+  awk -v row_number="$row_number" -v new_row="$new_row" 'NR == row_number { print new_row; next } { print }' "$table_name_path" > temp.txt && mv temp.txt "$table_name_path"
 
-  fi
-fi
+  echo "-------------------------------------"
+  echo "The Row is Updated Successfully"
+  echo "-------------------------------------"
+}
 
-./table_menu.sh $database_name
+update_record() {
+  list_available_tables
+  get_table_name
+  check_valid_table_name
+  check_table_exists
+  create_table_meta_path
+  check_data_presence
+  get_metadata_and_datatypes
+  get_pk_name
+  get_pk_index
+  get_pk_values
+  get_pk_dtype
+  get_old_row
+  create_new_row
+  ./table_menu.sh $database_name
+}
+
+update_record
