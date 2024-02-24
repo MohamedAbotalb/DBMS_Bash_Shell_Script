3b323a07b37c3b3c1e2223218b99b17959803148
Ahmed_Ebrahem <74773018+A7medIbra@users.noreply.github.com>
2024-02-24 10:08:35 +0200

    Update check_valid_value.sh

--- check_valid_value.sh
+++ check_valid_value.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Check if the input contains space or not
-# Use $# (number of arguments)
-if [[ $# -gt 1 ]];
+if [[ $# -gt 1 ]]
 then
+  # If there are more than 1 arguments, it means there is a space in the input name
   echo "Invalid input, please don't use space in the name"
 fi
 
@@ -11,7 +11,12 @@ fi
 value=$1
 pattern="^[a-zA-Z]{2,}[a-zA-Z0-9_]*$"
 
+# The pattern checks for the following conditions:
+# - The input value should start with at least 2 alphabetic characters (uppercase or lowercase)
+# - The remaining characters can be alphanumeric (letters or numbers) or an underscore (_)
+
 if [[ ! $value =~ $pattern ]]
 then
+  # If the input value does not match the pattern, it is considered invalid
   echo "Invalid input, please enter a valid value"
 fi
