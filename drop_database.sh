9a95ef5375da27681bf41388ae0d29d2408aebea
Ahmed_Ebrahem <74773018+A7medIbra@users.noreply.github.com>
2024-02-24 10:23:10 +0200

    Update drop_database.sh

--- drop_database.sh
+++ drop_database.sh
@@ -1,14 +1,28 @@
 #!/bin/bash
 
+# Source the database_functions.sh file to use the functions defined in it
 source database_functions.sh
 
+# Define the main function
 main() {
+  # List all databases
   list_databases
+
+  # Get the database name from the user
   get_database_name
+
+  # Check if the provided database name is valid
   check_valid_database
+
+  # Check if the database exists and is ready to connect
   check_database_presence_to_connect
-  confirm_delete_database 
+
+  # Confirm if the user wants to delete the database
+  confirm_delete_database
+
+  # Run the main script again
   ./main.sh
 }
 
+# Call the main function
 main
